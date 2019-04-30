3c528938b855d9bbfb3604054605dd043355fc50
Brandon Chen <brandon.chen@stonybrook.edu>
2019-04-30 04:06:03 +0000

    Added index page and controller

---
+++ controllers/form_controller.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    $('#add').submit(function (ev) {
+        ev.preventDefault();
+        $.ajax({
+            type: 'POST',
+            data : $(this).serialize(),
+            url: '/adduser',
+            
+            success: function(response) {
+                if (response.status === "OK") {
+                    $.ajax({
+                        type: 'GET',
+                        url: '/ui/verify',
+                        success: function(response) {
+                            $("html").html(response);
+                        }
+                    });
+                }
+
+            }
+
+        });
+    });
+
+    $('#verify').submit(function () {
+        ev.preventDefault();
+        $.ajax({
+            type: 'GET',
+            url: '/ui/verify',
+            success: function(response) {
+                $("html").html(response);
+            }
+        });
+        $.ajax({
+            type: 'POST',
+            data : $(this).serialize(),
+            url: '/../verify',
+        });
+    });
+    
+    $('#login').submit(function () {
+        $.ajax({
+            type: 'POST',
+            data : $(this).serialize(),
+            url: '/../login'
+        });
+    });
+});
